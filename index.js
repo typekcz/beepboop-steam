3e9fa3d9d82d9440acee9fcdbdaefafda02a320c
Lukáš Kotržena <lkotrzena@gmail.com>
2018-09-11 13:29:55 +0200

    test

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ var http = require('http');
 const puppeteer = require('puppeteer');
 
 http.createServer(async function (req, res) {
-	const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-	const page = await browser.newPage();
-	await page.goto('https://google.com');
-	let img = await page.screenshot({type: "jpeg", encoding: "base64"});
-	res.write('<img src="data:image/png;base64,' + img + '">');
-	res.end();
-	await browser.close();
+	try {
+		const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+		const page = await browser.newPage();
+		await page.goto('https://google.com');
+		let img = await page.screenshot({type: "jpeg", encoding: "base64"});
+		res.write('<img src="data:image/png;base64,' + img + '">');
+		res.end();
+		await browser.close();
+	} catch(e){
+		console.log(e);
+	}
 }).listen(process.env.PORT || 8080); //the server object listens on port 8080
