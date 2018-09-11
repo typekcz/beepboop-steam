9e8f7da07328a2b8a6e6ec7ad02ea431ba7aff18
Lukáš Kotržena <lkotrzena@gmail.com>
2018-09-11 12:24:50 +0200

    test

---
+++ index.js
@@ -0,0 +1,155 @@
+const puppeteer = require("puppeteer");
+const SteamChat = require("./steamchat");
+
+const args = process.argv.slice(2);
+
+let steamUserName = null;
+let steamPassword = null;
+let groupName = null;
+let channelName = null;
+
+if(typeof(process.env.STEAM_USER) !== "undefined")
+	steamUserName = process.env.STEAM_USER;
+if(typeof(process.env.STEAM_PASSWORD) !== "undefined")
+	steamPassword = process.env.STEAM_PASSWORD;
+if(typeof(process.env.GROUP_NAME) !== "undefined")
+	groupName = process.env.GROUP_NAME;
+if(typeof(process.env.CHANNEL_NAME) !== "undefined")
+	channelName = process.env.CHANNEL_NAME;
+
+for(let i = 0; i < args.length; i++) {
+	let arg = args[i];
+	
+	if((arg == "--steamuser" || arg == "-u") && args.length >= i){
+		steamUserName = args[++i];
+	} else if((arg == "--steampassword" || arg == "-p") && args.length >= i){
+		steamPassword = args[++i];
+	} else if((arg == "--groupname" || arg == "-g") && args.length >= i){
+		groupName = args[++i];
+	} else if((arg == "--channelname" || arg == "-c") && args.length >= i){
+		channelName = args[++i];
+	}
+}
+
+(async () => {
+	try{
+		const browser = await puppeteer.launch({
+			headless: false,
+			args: [
+				"--disable-client-side-phishing-detection",
+				"--disable-sync",
+				"--use-fake-ui-for-media-stream",
+				"--use-fake-device-for-media-stream",
+				"--enable-local-file-accesses",
+				"--allow-file-access-from-files",
+				"--disable-web-security",
+				"--reduce-security-for-testing"
+			]
+		});
+		const page = (await browser.pages())[0];
+		
+		page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+		page.on('pageerror', error => {
+			console.log(error.message());
+		});
+		/*page.on('response', response => {
+			console.log(response.status(), response.url);
+		});*/
+		page.on('requestfailed', request => {
+			console.log(request.failure().errorText, request.url);
+		});
+		
+		await page.setBypassCSP(true);
+		await page.goto("https://steamcommunity.com/chat", {waitUntil : "networkidle0"});
+		if(page.url().includes("login")){
+			try {
+				let navigationPromise = page.waitForNavigation({waitUntil : "networkidle0"});
+				await page.evaluate((user, pass) => {
+					document.querySelector('#steamAccountName').value = user;
+					document.querySelector('#steamPassword').value = pass;
+					document.querySelector('#SteamLogin').click();
+				}, steamUserName, steamPassword);
+				await navigationPromise;
+			} catch(e){
+				console.log(e);
+			}
+		}
+		
+		let steamchat = new SteamChat(page);
+		await steamchat.initAudio();
+		await new Promise((res) => { setTimeout(res, 1000) });
+		await steamchat.joinVoiceChannel(groupName, channelName);
+		await steamchat.playUrl("http://localhost:8080/engineerremix.mp3");
+		
+		var stdin = process.openStdin();
+
+		stdin.addListener("data", async function(d) {
+			console.log(await page.evaluate((str) => {
+				return eval(str);
+			}, d.toString().trim()));
+		});
+		
+		
+		/*await page.evaluate((groupName, channelName) => {
+			var audioContext = new AudioContext();
+			var mixedAudio = audioContext.createMediaStreamDestination();
+
+			function addStream(stream){
+				console.log(stream);
+				var audioSource = audioContext.createMediaStreamSource(stream);
+				audioSource.connect(mixedAudio);
+			}
+
+			navigator.getUserMedia = function(options, success, failure){
+				success(mixedAudio.stream);
+			}
+
+			document.getElementById("friendslist-container").style.width = "calc(100% - 150px)";
+			var beepboop = document.createElement("DIV");
+			beepboop.style.width = "150px";
+			beepboop.style.position = "absolute";
+			beepboop.style.right = "0";
+			beepboop.style.top = "0";
+			beepboop.style.bottom = "0";
+			document.body.appendChild(beepboop);
+
+			var file = document.createElement("INPUT");
+			file.type = "file";
+			file.onchange = () => {
+				audio.src = URL.createObjectURL(file.files[0]);
+			}
+			beepboop.appendChild(file);
+
+			var audio = document.createElement("AUDIO");
+			audio.setAttribute("controls","");
+			audio.muted = true;
+			audio.oncanplay = ()=>{
+				addStream(audio.captureStream());
+				audio.play();
+			}
+			beepboop.appendChild(audio);
+
+			setTimeout(()=>{
+				for(let g of document.querySelectorAll(".ChatRoomList .ChatRoomListGroupItem")){
+					if(g.querySelector(".chatRoomName").innerText == groupName){
+						let voiceRooms = g.querySelector(".ChatRoomListGroupItemChatRooms").firstChild;
+						if(voiceRooms.children.length == 0)
+							g.querySelector(".openGroupButton").click();
+						for(let ch of g.querySelectorAll(".chatRoomVoiceChannel")){
+							if(ch.querySelector(".chatRoomVoiceChannelName").innerText == channelName){
+								ch.click();
+								break;
+							}
+						}
+						break;
+					}
+				}
+			}, 1000);
+		}, groupName, channelName);*/
+
+		console.log("Done!");
+		//await browser.close();
+	}catch(e){
+		console.log(e);
+	}
+})();
---
+++ steamchat.js
@@ -0,0 +1,61 @@
+class SteamChat {
+	/**
+	 * @param {Page} page - Puppeteer page
+	 */
+	constructor(page){
+		this.page = page;
+	}
+	
+	initAudio(){
+		return this.page.evaluate(() => {
+			window.audioContext = new AudioContext();
+			window.mixedAudio = window.audioContext.createMediaStreamDestination();
+
+			function addStream(stream){
+				let audioSource = window.audioContext.createMediaStreamSource(stream);
+				audioSource.connect(window.mixedAudio);
+			}
+			window.addStream = addStream
+
+			navigator.getUserMedia = function(options, success, failure){
+				success(window.mixedAudio.stream);
+			}
+			
+			window.audio = new Audio();
+			window.audio.controls = true;
+			window.audio.muted = true;
+			window.audio.crossOrigin = "annonymous";
+			window.audio.oncanplay = ()=>{
+				window.addStream(audio.captureStream());
+				window.audio.play();
+			}
+		});
+	}
+	
+	joinVoiceChannel(group, channel){
+		return this.page.evaluate((groupName, channelName) => {
+			for(let g of document.querySelectorAll(".ChatRoomList .ChatRoomListGroupItem")){
+				if(g.querySelector(".chatRoomName").innerText == groupName){
+					let voiceRooms = g.querySelector(".ChatRoomListGroupItemChatRooms").firstChild;
+					if(voiceRooms.children.length == 0)
+						g.querySelector(".openGroupButton").click();
+					for(let ch of g.querySelectorAll(".chatRoomVoiceChannel")){
+						if(ch.querySelector(".chatRoomVoiceChannelName").innerText == channelName){
+							ch.click();
+							break;
+						}
+					}
+					break;
+				}
+			}
+		}, group, channel);
+	}
+	
+	playUrl(url){
+		return this.page.evaluate((url) => {
+			window.audio.src = url;
+		}, url);
+	}
+}
+
+module.exports = SteamChat;
