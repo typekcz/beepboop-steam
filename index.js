4647dce1d491ec25ba320d912a6efd5512d8c9f1
Lukáš Kotržena <lkotrzena@gmail.com>
2018-09-11 13:27:25 +0200

    test

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ http.createServer(async function (req, res) {
 	const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 	const page = await browser.newPage();
 	await page.goto('https://google.com');
-	let buf = await page.screenshot({type: "jpeg"});
-	res.pipe(buf);
+	let img = await page.screenshot({type: "jpeg", encoding: "base64"});
+	res.write('<img src="data:image/png;base64,' + img + '">');
 	res.end();
 	await browser.close();
 }).listen(process.env.PORT || 8080); //the server object listens on port 8080
