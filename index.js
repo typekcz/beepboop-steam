34b2cfd0612f54ac03bf27ae1b791b8f57191198
Lukáš Kotržena <lkotrzena@gmail.com>
2020-12-14 20:54:16 +0100

    Omfg

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ class Main {
 
 		try {
 			const browser = await puppeteer.launch({
-				headless: false,
+				headless: true,
 				args: [
 					"--disable-client-side-phishing-detection",
 					"--disable-sync",
